34c50031f33699f898ddbdfc4920b53ee8cf96e0
Hank-Choi <zlzlqlzl1@naver.com>
2022-01-23 03:07:41 +0900

    안쓰는 주석 제거

--- src/main/kotlin/com/wafflestudio/snuttev/domain/tag/repository/TagRepository.kt
+++ src/main/kotlin/com/wafflestudio/snuttev/domain/tag/repository/TagRepository.kt
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
 interface TagRepository : JpaRepository<Tag, Long> {
-//    @Query("SELECT t FROM Tag t WHERE t.id IN :tagIdList")
     @Query("SELECT t FROM Tag t JOIN FETCH t.tagGroup WHERE t.id IN :tagIdList")
     fun getTagsWithTagGroupByTagsIdIsIn(tagIdList: List<Long>): List<Tag>
 }
