c91133794fc7986ffa6d941629b7c6e552d71745
Davin <bdv111@snu.ac.kr>
2022-01-29 12:18:58 +0900

    add enum class LectureClassification (not applied)

--- src/main/kotlin/com/wafflestudio/snuttev/domain/lecture/model/Lecture.kt
+++ src/main/kotlin/com/wafflestudio/snuttev/domain/lecture/model/Lecture.kt
@@ -32,6 +32,19 @@ class Lecture(
     val semesterLectures: List<SemesterLecture> = listOf()
 ) : BaseEntity()
 
+
+enum class LectureClassification(@get:JsonValue val value: String) {
+    LIBERAL_EDUCATION("교양"),
+    ELECTIVE_SUBJECT("전선"),
+    REQUISITE_SUBJECT("전필"),
+    ELECTIVE_GENERAL("일선"),
+    READING_AND_RESEARCH("논문"),
+    TEACHING_CERTIFICATION("교직"),
+    GRADUATE("대학원"),
+    CORE_SUBJECT("공통"),
+}
+
+
 data class LectureEvaluationSummaryDao(
     val id: Long?,
 
