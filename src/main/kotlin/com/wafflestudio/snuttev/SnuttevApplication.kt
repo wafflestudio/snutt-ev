ab33e89d02486eea03da7403fb48ba580b381ad5
Hank-Choi <zlzlqlzl1@naver.com>
2022-07-04 10:59:35 +0900

    Change batch job to turn off

--- src/main/kotlin/com/wafflestudio/snuttev/SnuttevApplication.kt
+++ src/main/kotlin/com/wafflestudio/snuttev/SnuttevApplication.kt
@@ -7,7 +7,7 @@ import org.springframework.cache.annotation.EnableCaching
 
 @SpringBootApplication
 @EnableCaching
-@EnableBatchProcessing
+//@EnableBatchProcessing
 class SnuttevApplication
 
 fun main(args: Array<String>) {
