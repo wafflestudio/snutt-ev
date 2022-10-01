6306acf140cc84c38da972778b1898ecf41752c7
davin111 <bdv111@snu.ac.kr>
2022-10-02 00:07:45 +0900

    my evaluations page api

---
+++ core/src/test/kotlin/com/wafflestudio/snuttev/core/SnuttevCoreTestApplication.kt
@@ -0,0 +1,11 @@
+package com.wafflestudio.snuttev.core
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnuttevCoreTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<SnuttevCoreTestApplication>(*args)
+}
