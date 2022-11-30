5037a63c29bbe535d31669b2722ea111eaf2a478
davin111 <bdv111@snu.ac.kr>
2022-11-30 09:34:17 +0900

    rm NotNull

--- core/src/main/kotlin/com/wafflestudio/snuttev/core/domain/evaluation/dto/EvaluationRequest.kt
+++ core/src/main/kotlin/com/wafflestudio/snuttev/core/domain/evaluation/dto/EvaluationRequest.kt
@@ -2,29 +2,22 @@ package com.wafflestudio.snuttev.core.domain.evaluation.dto
 
 import org.hibernate.validator.constraints.Range
 import javax.validation.constraints.NotBlank
-import javax.validation.constraints.NotNull
 
 data class CreateEvaluationRequest(
-    @field:NotNull
     val content: String,
 
-    @field:NotNull
     @field:Range(min = 0, max = 10)
     val gradeSatisfaction: Double,
 
-    @field:NotNull
     @field:Range(min = 0, max = 10)
     val teachingSkill: Double,
 
-    @field:NotNull
     @field:Range(min = 0, max = 10)
     val gains: Double,
 
-    @field:NotNull
     @field:Range(min = 0, max = 10)
     val lifeBalance: Double,
 
-    @field:NotNull
     @field:Range(min = 0, max = 10)
     val rating: Double,
 )
