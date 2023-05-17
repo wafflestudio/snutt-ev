28d5f5f5d86b990f6eb531f85ce6ade2f05e0224
davin111 <bdv111@snu.ac.kr>
2023-05-17 20:21:03 +0900

    modify swagger dep

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -3,6 +3,5 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.springdoc:springdoc-openapi-ui:1.7.0")
-    implementation("javax.xml.bind:jaxb-api:2.3.1")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
 }
