8659401fc1ee3aded5c7d5980c45bbcd8585f548
Chanyeong Lim <dlacksdud2@snu.ac.kr>
2024-06-06 02:09:45 +0900

    snuttId 마다 evLectureId 보내게 수정

--- core/src/main/kotlin/com/wafflestudio/snuttev/core/domain/lecture/service/LectureService.kt
+++ core/src/main/kotlin/com/wafflestudio/snuttev/core/domain/lecture/service/LectureService.kt
@@ -133,15 +133,15 @@ class LectureService(
 
     fun getEvLectureSummaryForSnutt(semesterLectureSnuttIds: List<String>): List<EvLectureSummaryForSnutt> {
         val snuttIdLectureIdMap = snuttLectureIdMapRepository.findAllWithSemesterLectureBySnuttIdIn(semesterLectureSnuttIds)
-            .associate { it.semesterLecture.lecture.id!! to it.snuttId }
-        val lectureIds = snuttIdLectureIdMap.keys
+            .associate { it.snuttId to it.semesterLecture.lecture.id!! }
+        val lectureIds = snuttIdLectureIdMap.values
         val evMap = lectureRepository.findAllRatingsByLectureIds(lectureIds).associateBy { it.id }
-        return lectureIds.map {
+        return snuttIdLectureIdMap.map {
             EvLectureSummaryForSnutt(
-                snuttId = snuttIdLectureIdMap[it]!!,
-                evLectureId = it,
-                avgRating = evMap[it]?.avgRating,
-                evaluationCount = evMap[it]?.count ?: 0L,
+                snuttId = it.key,
+                evLectureId = it.value,
+                avgRating = evMap[it.value]?.avgRating,
+                evaluationCount = evMap[it.value]?.count ?: 0L,
             )
         }
     }
