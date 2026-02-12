ff0f2c2c8121b8ce136a6ccd41ae12fbd935242b
Chanyeong Lim <asp345@posteo.net>
2026-02-12 13:14:00 +0900

    switch to oci secrets

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -15,9 +15,6 @@ dependencies {
 
     implementation("org.flywaydb:flyway-core:11.13.2")
     implementation("org.flywaydb:flyway-mysql:11.13.2")
-    implementation("software.amazon.awssdk:secretsmanager:2.34.6")
-    implementation("software.amazon.awssdk:sts:2.34.6")
-
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
 
     runtimeOnly("com.mysql:mysql-connector-j")
