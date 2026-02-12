7acdc5fdeb365ef93b23821faae56f02e5f58c61
Chanyeong Lim <asp345@posteo.net>
2026-02-12 17:41:48 +0900

    remove modelResolver

--- api/src/main/kotlin/com/wafflestudio/snuttev/config/SwaggerConfig.kt
+++ api/src/main/kotlin/com/wafflestudio/snuttev/config/SwaggerConfig.kt
@@ -1,7 +1,5 @@
 package com.wafflestudio.snuttev.config
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import io.swagger.v3.core.jackson.ModelResolver
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
 import org.springframework.context.annotation.Bean
@@ -9,9 +7,6 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class SwaggerConfig {
-    @Bean
-    fun modelResolver(objectMapper: ObjectMapper): ModelResolver = ModelResolver(objectMapper)
-
     @Bean
     fun openAPI(): OpenAPI =
         OpenAPI().info(
